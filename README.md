8e25e7d89e4e4934b711481262b5ce820ce3f175
maxim-ge <maxim.ge@gmail.com>
2019-03-27 12:53:29 +0300

    ProvideKeyValue for slice

--- README.md
+++ README.md
@@ -4,15 +4,58 @@ Go dependency injection for functions (and not only...)
 
 # Usage
 
-Provide key-value
+## Provide key-value
 
 - Declare: `var MyMap map[string]int`
-- Provide : `godif.ProvideKeyValue(&MyMap, "key1", 1)`
+- Implement
+  - Manually: `MyMap = map[string]int{}`
+    - `godif.Provide(&MyMap, map[string]int{})` -> Implementation provided for non-nil error
+  - Provide implementation: `godif.Provide(&MyMap, map[string]int{})`
+  - No implementation -> error
+- Provide data: `godif.ProvideKeyValue(&MyMap, "key1", 1)`
+- Multiple values per key error:
+  - `godif.ProvideKeyValue(&MyMap, "key1", 1)`
+  - `godif.ProvideKeyValue(&MyMap, "key1", 2)`
+- Incompatible types error:
+  - `godif.ProvideKeyValue(&MyMap, "key1", "str")`
 
-Provide slice element
+## Provide key-slice
+
+- Declare: `var MyMap map[string][]int`
+- Implement
+  - Manually: `MyMap = map[string][]int{}`
+    - `godif.Provide(&MyMap, map[string][]int{})` -> Implementation provided for non-nil error
+  - Provide implementation: `godif.Provide(&MyMap, map[string][]int{})`
+  - No implementation -> error
+- Add initial data if needed: `MyMap["key1"] = append(MyMap["key1"], 42)`
+  - Further `godif.ProvideKeyValue()` calls will append data to the existing slice
+- Provide data: 
+  - `godif.ProvideKeyValue(&MyMap, "key1", 1)`
+  - `godif.ProvideKeyValue(&MyMap, "key1", 2)`
+  - `godif.ProvideKeyValue(&MyMap, "key1", []int{3, 4})`
+- Incompatible types error:
+  - `godif.ProvideKeyValue(&MyMap, "key1", "str")`
+
+## Provide slice element
 
 - Declare: `var MySlice []string`
-- Provide : `godif.ProvideSliceElement(&MySlice, "str1")`
+- Implement: `godif.Provide(&MySlice, []string{})`
+- Implement
+  - Manually: `MySlice = []string{}`
+    - `godif.Provide(&MySlice, []string{})` -> Implementation provided for non-nil error
+  - Provide implementation: `godif.Provide(&MySlice, []string{})`
+  - No implementation -> Implementation not provided error
+- Add initial data if needed: `MySlice = append(MySlice, 42)`
+  - Further `godif.ProvideSliceElement()` calls will append data to the existing slice
+- Provide data: 
+  - `godif.ProvideSliceElement(&MySlice, "str1")`
+  - `godif.ProvideSliceElement(&MySlice, []string{"str3", "str4"})`
+
+## Reset all injections
+- `godif.Reset()`
+- Provided vars will be nilled
+- Manually inited vars will be kept
+- Data injected into manually inited vars will be kept
 
 # Usage Example
 
--- godif.go
+++ godif.go
@@ -12,15 +12,21 @@ import (
 	"runtime"
 )
 
-type srcElem struct {
+type src struct {
 	file string
 	line int
+}
+
+type srcElem struct {
+	*src
 	elem interface{}
 }
 
 var required []*srcElem
 var provided map[interface{}][]*srcElem
 var keyValues map[interface{}]map[interface{}][]*srcElem
+var sliceElements map[interface{}][]*srcElem
+var resolveSrc *src = nil
 
 func init() {
 	createVars()
@@ -29,55 +35,66 @@ func init() {
 func createVars() {
 	provided = make(map[interface{}][]*srcElem)
 	keyValues = make(map[interface{}]map[interface{}][]*srcElem)
+	sliceElements = make(map[interface{}][]*srcElem)
+}
+
+func newSrcElem(file string, line int, elem interface{}) *srcElem {
+	return &srcElem{&src{file, line}, elem}
 }
 
 // Reset clears all assignations
 func Reset() {
-	createVars()
-	if required != nil {
-		for _, r := range required {
-			v := reflect.ValueOf(r.elem)
-			if v.Kind() == reflect.Ptr {
-				v = v.Elem()
-				if v.CanSet() {
-					v.Set(reflect.Zero(v.Type()))
-				}
+	for _, r := range required {
+		v := reflect.ValueOf(r.elem)
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+			if v.CanSet() {
+				v.Set(reflect.Zero(v.Type()))
 			}
 		}
-		required = make([]*srcElem, 0)
 	}
+	for p, _ := range provided {
+		v := reflect.ValueOf(p)
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+			if v.CanSet() {
+				v.Set(reflect.Zero(v.Type()))
+			}
+		}
+	}
+	required = make([]*srcElem, 0)
+	resolveSrc = nil
+	createVars()
 }
 
 // ProvideSliceElement s.e.
 func ProvideSliceElement(pointerToSlice interface{}, element interface{}) {
-
+	_, file, line, _ := runtime.Caller(1)
+	if sliceElements[pointerToSlice] == nil {
+		sliceElements[pointerToSlice] = make([]*srcElem, 0)
+	}
+	sliceElements[pointerToSlice] = append(sliceElements[pointerToSlice], newSrcElem(file, line, element))
 }
 
 // ProvideKeyValue s.e.
 func ProvideKeyValue(pointerToMap interface{}, key interface{}, value interface{}) {
-	//requireEx(pMap, 2)
 	_, file, line, _ := runtime.Caller(1)
 	if keyValues[pointerToMap] == nil {
 		keyValues[pointerToMap] = make(map[interface{}][]*srcElem)
 	}
-	keyValues[pointerToMap][key] = append(keyValues[pointerToMap][key], &srcElem{file, line, value})
+	keyValues[pointerToMap][key] = append(keyValues[pointerToMap][key], newSrcElem(file, line, value))
 }
 
 // Provide registers implementation of ref type
 func Provide(ref interface{}, funcImplementation interface{}) {
 	_, file, line, _ := runtime.Caller(1)
-	provided[ref] = append(provided[ref], &srcElem{file, line, funcImplementation})
+	provided[ref] = append(provided[ref], newSrcElem(file, line, funcImplementation))
 }
 
 // Require registers dep
 func Require(toInject interface{}) {
-	requireEx(toInject, 2)
-}
-
-// Require registers dep
-func requireEx(toInject interface{}, callerStackOffset int) {
-	_, file, line, _ := runtime.Caller(callerStackOffset)
-	required = append(required, &srcElem{file, line, toInject})
+	_, file, line, _ := runtime.Caller(1)
+	required = append(required, newSrcElem(file, line, toInject))
 }
 
 // ResolveAll all deps
@@ -87,26 +104,79 @@ func ResolveAll() Errors {
 		return errs
 	}
 
-	for _, reqVar := range required {
-		impls := provided[reqVar.elem]
-		reqValue := reflect.ValueOf(reqVar.elem).Elem()
-		reqValue.Set(reflect.ValueOf(impls[0].elem))
-	}
-	for _, reqVar := range required {
-		mapToAppend := keyValues[reqVar.elem]
-		for k, v := range mapToAppend {
-			dataValue := reflect.ValueOf(v[0].elem)
-			reqValue := reflect.ValueOf(reqVar.elem).Elem()
-			keyValue := reflect.ValueOf(k)
-			reqValue.SetMapIndex(keyValue, dataValue)
+	for target, provVar := range provided {
+		targetValue := reflect.ValueOf(target).Elem()
+		if targetValue.IsNil() {
+			targetValue.Set(reflect.ValueOf(provVar[0].elem))
 		}
 	}
+
+	for targetMap, kvToAppend := range keyValues {
+		targetMapType := reflect.TypeOf(targetMap).Elem()
+		tragetMapValueType := targetMapType.Elem()
+		tragetMapValueKind := tragetMapValueType.Kind()
+		targetMapValue := reflect.ValueOf(targetMap).Elem()
+		for k, v := range kvToAppend {
+			keyValue := reflect.ValueOf(k)
+			var toAppendValue reflect.Value
+			if isSlice(tragetMapValueKind) {
+				existingSlice := targetMapValue.MapIndex(keyValue)
+				newSlice := reflect.New(reflect.SliceOf(tragetMapValueType.Elem())).Elem()
+				if existingSlice.IsValid() {
+					for i := 0; i < existingSlice.Len(); i++ {
+						existingElement := existingSlice.Index(i)
+						newSlice.Set(reflect.Append(newSlice, existingElement))
+					}
+				}
+				for _, elementToAppend := range v {
+					elementToAppendValue := reflect.ValueOf(elementToAppend.elem)
+					elementToAppendKind := elementToAppendValue.Kind()
+					if isSlice(elementToAppendKind) {
+						for i := 0; i < elementToAppendValue.Len(); i++ {
+							newSlice.Set(reflect.Append(newSlice, elementToAppendValue.Index(i)))
+						}
+					} else {
+						newSlice.Set(reflect.Append(newSlice, elementToAppendValue))
+					}
+				}
+				toAppendValue = newSlice
+			} else {
+				toAppendValue = reflect.ValueOf(v[0].elem)
+			}
+			targetMapValue.SetMapIndex(keyValue, toAppendValue)
+		}
+	}
+
+	for targetSlice, elementsToAppend := range sliceElements {
+		targateSliceValue := reflect.ValueOf(targetSlice).Elem()
+		for _, elementToAppend := range elementsToAppend {
+			elementValue := reflect.ValueOf(elementToAppend.elem)
+			elementKind := elementValue.Kind()
+			if isSlice(elementKind) {
+				for i := 0; i < elementValue.Len(); i++ {
+					targateSliceValue.Set(reflect.Append(targateSliceValue, elementValue.Index(i)))
+				}
+			} else {
+				targateSliceValue.Set(reflect.Append(targateSliceValue, elementValue))
+			}
+		}
+	}
+
+	_, file, line, _ := runtime.Caller(1)
+	resolveSrc = &src{file, line}
 
 	return nil
 }
 
+func isSlice(kind reflect.Kind) bool {
+	return kind == reflect.Array || kind == reflect.Slice
+}
+
 func getErrors() Errors {
 	var errs Errors
+	if resolveSrc != nil {
+		return []error{&EAlreadyResolved{resolveSrc}}
+	}
 	for _, req := range required {
 
 		impls := provided[req.elem]
@@ -129,24 +199,90 @@ func getErrors() Errors {
 		for _, impl := range impls {
 			implType := reflect.TypeOf(impl.elem)
 			if !implType.AssignableTo(reqType) {
-				errs = append(errs, &EIncompatibleTypes{req, impls[0]})
+				errs = append(errs, &EIncompatibleTypesFunc{req, impls[0]})
 			}
 		}
+	}
 
-		for _, v := range keyValues[req.elem] {
-			if len(v) > 1 {
-				errs = append(errs, &EMultipleValues{req, v})
+	for targetMap, kvToAppend := range keyValues {
+		targetMapType := reflect.TypeOf(targetMap).Elem()
+		targetMapValue := reflect.ValueOf(targetMap).Elem()
+		impl := provided[targetMap]
+		if targetMapValue.IsNil() {
+			if impl == nil {
+				keys := reflect.ValueOf(kvToAppend).MapKeys()
+				errs = append(errs, &EImplementationNotProvided{kvToAppend[keys[0].Interface()][0]})
+				continue
+			}
+		} else {
+			if impl != nil {
+				errs = append(errs, &EImplementationProvidedForNonNil{impl[0]})
+				continue
+			}
+		}
+		tragetMapValueType := targetMapType.Elem()
+		tragetMapValueKind := tragetMapValueType.Kind()
+		for _, v := range kvToAppend {
+			if isSlice(tragetMapValueKind) {
+				reqMapValueSliceElementType := tragetMapValueType.Elem()
+				for _, provElement := range v {
+					provType := reflect.TypeOf(provElement.elem)
+					provKind := provType.Kind()
+					if isSlice(provKind) {
+						provType = provType.Elem()
+					}
+					if !provType.AssignableTo(reqMapValueSliceElementType) {
+						errs = append(errs, &EIncompatibleTypesSlice{targetMapType, provElement})
+					}
+				}
 			} else {
-				vType := reflect.TypeOf(v[0].elem)
-				if !vType.AssignableTo(reqType.Elem()) {
-					errs = append(errs, &EIncompatibleTypes{req, v[0]})
+				if len(v) > 1 {
+					errs = append(errs, &EMultipleValues{v})
+				} else {
+					vType := reflect.TypeOf(v[0].elem)
+					if !vType.AssignableTo(tragetMapValueType) {
+						errs = append(errs, &EIncompatibleTypesSlice{tragetMapValueType, v[0]})
+					}
 				}
 			}
 		}
 	}
+
+	for targetSlice, elementsToAppend := range sliceElements {
+		targetSliceType := reflect.TypeOf(targetSlice).Elem()
+		targetSliceValue := reflect.ValueOf(targetSlice).Elem()
+		impl := provided[targetSlice]
+		if targetSliceValue.IsNil() {
+			if impl == nil {
+				errs = append(errs, &EImplementationNotProvided{elementsToAppend[0]})
+				continue
+			}
+		} else {
+			if impl != nil {
+				errs = append(errs, &EImplementationProvidedForNonNil{impl[0]})
+				continue
+			}
+		}
+		for _, v := range elementsToAppend {
+			vType := reflect.TypeOf(v.elem)
+			vKind := vType.Kind()
+			if isSlice(vKind) {
+				vType = vType.Elem()
+			}
+			if !vType.AssignableTo(targetSliceType.Elem()) {
+				errs = append(errs, &EIncompatibleTypesSlice{targetSliceType, v})
+			}
+		}
+	}
+
+	// funcs only
 	for provVar, provSrcs := range provided {
+		if reflect.TypeOf(provVar).Elem().Kind() != reflect.Func {
+			continue
+		}
 		var found = false
 		for _, reqVar := range required {
+
 			if reqVar.elem == provVar {
 				found = true
 				break
